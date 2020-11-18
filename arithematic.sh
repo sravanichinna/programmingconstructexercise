e722fc9db9a5d7f86b09ae0020d35cadcf717866
sravanichinna <sravanidane413.com>
2020-11-18 11:46:03 +0530

    added: flipcoin logic

--- arithematic.sh
+++ arithematic.sh
@@ -38,3 +38,23 @@ fi
 done
 done
 echo "sort in ascending order: " ${arr[@]}
+
+
+
+#!/bin/bash
+headCount=0
+tailCount=0
+while(( $headCount != 21 && $tailCount != 21 ))
+do
+flip=$(( $RANDOM%2 ))
+if [[ $flip -eq 1 ]]
+then
+echo -n "heads"
+((headCount++))
+else
+echo -n "tails"
+((tailCount++))
+fi
+done
+echo "total no of heads:" $headCount
+echo "total no of tails:" $tailCount
